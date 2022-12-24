663eab2bd8dcbde903205f7db8e1fe460708e5f2
cofob <cofob@riseup.net>
2022-12-25 02:52:55 +0700

    Fix

--- web3-storage/web3_storage/__main__.py
+++ web3-storage/web3_storage/__main__.py
@@ -2,9 +2,13 @@ from os import environ
 from .exporter import Web3StorageExporter
 
 
-if __name__ == "__main__":
+def main():
     token = environ["WEB3_STORAGE_TOKEN"]
     port = int(environ.get("WEB3_STORAGE_PORT", 9325))
     host = environ.get("WEB3_STORAGE_HOST", "0.0.0.0")
     exporter = Web3StorageExporter(token, port, host)
     exporter.start()
+
+
+if __name__ == "__main__":
+    main()
