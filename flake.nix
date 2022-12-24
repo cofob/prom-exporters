e4a475cb782c94a73898ed686740bafd3f8dc24e
cofob <cofob@riseup.net>
2022-12-25 02:43:27 +0700

    Add flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Prometheus exporters";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+  inputs.poetry2nix = {
+    url = "github:nix-community/poetry2nix";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    {
+      overlay = nixpkgs.lib.composeManyExtensions [
+        poetry2nix.overlay
+        (final: prev: {
+          exporters = {
+            web3-storage = prev.poetry2nix.mkPoetryApplication {
+              projectDir = ./web3-storage;
+            };
+          };
+        })
+      ];
+    } // (flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlay ];
+        };
+      in
+      {
+        packages = pkgs.exporters;
+      }));
+}
